b7faad5ed41801dc733b579e30a6c03a54d13fc3
frsmtv <fsimitchiev@gmail.com>
2017-02-06 17:50:48 +0100

    o

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+   // **** GADIENTIFY ****
+    $('body').gradientify({
+        gradients: [
+            { start: [49,76,172], stop: [242,159,191] },
+            { start: [255,103,69], stop: [240,154,241] },
+            { start: [33,229,241], stop: [235,236,117] }
+        ]
+    });
+
+
+
+
+
+});
